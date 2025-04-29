79532b3bee348c12da626769442a60d8efad9e49
Luis Macias <luismaciasantonio2000@gmail.com>
2025-04-28 21:22:33 -0500

    Avances de eliminacion de codigo ivalidos

--- src/app/layout/component/app.configurator.ts
+++ src/app/layout/component/app.configurator.ts
@@ -107,7 +107,6 @@ export class AppConfigurator {
 
     menuModeOptions = [
         { label: 'Static', value: 'static' },
-        { label: 'Overlay', value: 'overlay' }
     ];
 
     ngOnInit() {
@@ -151,23 +150,7 @@ export class AppConfigurator {
                 950: '#030712'
             }
         },
-        {
-            name: 'zinc',
-            palette: {
-                0: '#ffffff',
-                50: '#fafafa',
-                100: '#f4f4f5',
-                200: '#e4e4e7',
-                300: '#d4d4d8',
-                400: '#a1a1aa',
-                500: '#71717a',
-                600: '#52525b',
-                700: '#3f3f46',
-                800: '#27272a',
-                900: '#18181b',
-                950: '#09090b'
-            }
-        },
+    
         {
             name: 'neutral',
             palette: {
@@ -335,73 +318,15 @@ export class AppConfigurator {
         } else {
             if (preset === 'Nora') {
                 return {
-                    semantic: {
-                        primary: color.palette,
-                        colorScheme: {
-                            light: {
-                                primary: {
-                                    color: '{primary.600}',
-                                    contrastColor: '#ffffff',
-                                    hoverColor: '{primary.700}',
-                                    activeColor: '{primary.800}'
-                                },
-                                highlight: {
-                                    background: '{primary.600}',
-                                    focusBackground: '{primary.700}',
-                                    color: '#ffffff',
-                                    focusColor: '#ffffff'
-                                }
-                            },
-                            dark: {
-                                primary: {
-                                    color: '{primary.500}',
-                                    contrastColor: '{surface.900}',
-                                    hoverColor: '{primary.400}',
-                                    activeColor: '{primary.300}'
-                                },
-                                highlight: {
-                                    background: '{primary.500}',
-                                    focusBackground: '{primary.400}',
-                                    color: '{surface.900}',
-                                    focusColor: '{surface.900}'
-                                }
-                            }
-                        }
-                    }
+                   
                 };
             } else {
                 return {
                     semantic: {
                         primary: color.palette,
                         colorScheme: {
-                            light: {
-                                primary: {
-                                    color: '{primary.500}',
-                                    contrastColor: '#ffffff',
-                                    hoverColor: '{primary.600}',
-                                    activeColor: '{primary.700}'
-                                },
-                                highlight: {
-                                    background: '{primary.50}',
-                                    focusBackground: '{primary.100}',
-                                    color: '{primary.700}',
-                                    focusColor: '{primary.800}'
-                                }
-                            },
-                            dark: {
-                                primary: {
-                                    color: '{primary.400}',
-                                    contrastColor: '{surface.900}',
-                                    hoverColor: '{primary.300}',
-                                    activeColor: '{primary.200}'
-                                },
-                                highlight: {
-                                    background: 'color-mix(in srgb, {primary.400}, transparent 84%)',
-                                    focusBackground: 'color-mix(in srgb, {primary.400}, transparent 76%)',
-                                    color: 'rgba(255,255,255,.87)',
-                                    focusColor: 'rgba(255,255,255,.87)'
-                                }
-                            }
+                         
+                       
                         }
                     }
                 };
