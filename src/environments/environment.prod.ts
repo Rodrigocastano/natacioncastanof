a67a14ed695f6993f130cd9eed6ec7adebd04cb8
bermeo2000 <luismaciasantonio2000@gmail.com>
2025-10-28 20:37:36 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const  environment = {
     production: true, 
 
-     apisUrls: 'http://127.0.0.1:8000/api'
+     apisUrls: 'https://natacioncastanob-production.up.railway.app/api'
      
 }
